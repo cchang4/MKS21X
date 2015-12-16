72f74bcf38a5f969d24417295796243e32394d94
Catherine Chang <cchang4@stuy.edu>
2015-12-16 12:31:20 -0500

    more stuff

--- LabGUI/TempConversionWindow.java
+++ LabGUI/TempConversionWindow.java
@@ -1,3 +1,17 @@
-public class TempConversionWindow{
-    //add stuff later
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class TempConversionWindow extends JFrame implements ActionListener{
+    private Container pane;
+    private JLabel j;
+    private JTextField t;
+
+    public TempConversionWindow(){
+	this.setTitle("Temperature Converter");
+	this.setSize(600,400);
+	this.setLocation(100,100);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+    
 }
