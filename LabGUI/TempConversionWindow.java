20452761076df44bf504ab789863a2ea0275946d
Catherine Chang <cchang4@stuy.edu>
2015-12-16 23:11:36 -0500

    just the simple window popping up

--- LabGUI/TempConversionWindow.java
+++ LabGUI/TempConversionWindow.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class TempConversionWindow extends JFrame implements ActionListener{
+public class TempConversionWindow extends JFrame{
     private Container pane;
     private JLabel j;
     private JTextField t;
