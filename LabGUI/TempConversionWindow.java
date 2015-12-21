af8cfe9df5ee5856cb2fe1b26bed32ebb6658eea
Catherine Chang <cchang4@stuy.edu>
2015-12-20 21:33:15 -0500

    FINALLY FINISHED 4 days late...

--- LabGUI/TempConversionWindow.java
+++ LabGUI/TempConversionWindow.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class TempConversionWindow extends JFrame{
+public class TempConversionWindow extends JFrame implements ActionListener{
     private Container pane;
     private JButton b, b2;
     private JLabel l;
@@ -19,15 +19,44 @@ public class TempConversionWindow extends JFrame{
 	pane = this.getContentPane();
 	pane.setLayout(new GridLayout(4, 1));
 
-	b = new JButton("C to F");
-	b2 = new JButton("F to C");
-	l = new JLabel("",null,JLabel.CENTER);
-	t = new JTextField(3);
+	b = new JButton("\u00b0"+ "C to "+ "\u00b0"+ "F");
+	b.addActionListener(this);
+	b.setActionCommand(" F");
+	b2 = new JButton("\u00b0" +"F to "+ "\u00b0" +"C");
+	b2.addActionListener(this);
+	b2.setActionCommand(" C");
+	l = new JLabel("Convert any number into Farenheit or Celsius!",
+		       null,JLabel.CENTER);
+	t = new JTextField();
+	t.setHorizontalAlignment(JTextField.CENTER);
 
 	pane.add(t);
 	pane.add(b);
 	pane.add(b2);
 	pane.add(l);
+    }
+
+    public void actionPerformed(ActionEvent e){
+	String event = e.getActionCommand();
+	if(event.equals(" F")){
+	    try{
+		String s = t.getText();
+		double d = Double.parseDouble(s) * (9.0 / 5.0) + 32.0;
+		l.setText(String.valueOf(d) + "\u00b0" +"F");
+	    } catch (NumberFormatException nfe) {
+		l.setText("Enter a number");
+	    }
+	}
+	if(event.equals(" C")){
+	    try{
+		String s = t.getText();
+		double d = (Double.parseDouble(s) - 32.0) * (5.0 / 9.0);
+		l.setText(String.valueOf(d) + "\u00b0" +"C");
+	    } catch (NumberFormatException nfe) {
+		l.setText("Enter a number");
+    
+	    }
+	}
     }
 
 
